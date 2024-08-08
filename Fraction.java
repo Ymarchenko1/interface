3e252061ce84a42b94fff98d74fa3575a00e268b
Ymarchenko <35733@mail.ru>
2024-08-08 16:29:36 +0300

    добавил решение 6-го задания

--- Fraction.java
+++ Fraction.java
@@ -53,3 +53,19 @@ class Fraction extends Number{
     }
 }
 
+// Тут уже начинается задание 6
+class Start {
+    public static void main(String[] args) {
+        System.out.println(sumAll(2, new Fraction(3, 5), 2.3));
+        System.out.println(sumAll(3.6, new Fraction(49, 12), 3, new Fraction(3, 2)));
+        System.out.println(sumAll(new Fraction(1, 3), 1));
+    }
+
+    public static double sumAll(Number... nums) {
+        double sum = 0;
+        for (Number leng : nums) {
+            sum += leng.doubleValue();
+        }
+        return sum;
+    }
+}
